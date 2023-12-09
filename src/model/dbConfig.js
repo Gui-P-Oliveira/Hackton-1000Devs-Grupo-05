383049444e6fac097a52a354226642a16e8ecb4f
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-12-09 13:36:34 -0300

    chore: adciona querys para banco de dados

--- src/model/dbConfig.js
+++ src/model/dbConfig.js
@@ -14,6 +14,31 @@ const showVacinasCount = async() => {
     console.log(result.rows);
 }
 
+const cadastrarPaciente = async(nome, dataNascimento) => {
+    const data = new Date(dataNascimento);
+    const result = await (`INSERT INTO PACIENTE (nome, Data_nascimento) VALUES (${11}, ${nome}, ${data} )`)    
+}
+
+const cadastrarIdRede = async(tipoRede) => {    
+    const result = await (`INSERT INTO REDE (Tipo_rede) VALUES (${tipoRede})`)
+    
+}
+
+const consultaPaciente = async() => {
+    const result = await pool.query('SELECT * from PACIENTE');
+    console.log(result.rows);
+}
+
+const consultaRede = async() => {
+    const result = await pool.query('SELECT * from REDE');
+    console.log(result.rows);
+}
+
+cadastrarPaciente("Joao", "1988-12-09")
+cadastrarIdRede('Rede Maluca')
+consultaPaciente()
+consultaRede()
+
 module.exports = {
     showVacinasCount
 }
