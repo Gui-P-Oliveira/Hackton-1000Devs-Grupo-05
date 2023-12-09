6e9c1230d0ee24e66db833810eda8eefc59f1e86
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-12-09 13:38:57 -0300

    feat: adiciona rota de pessoas

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const userRoutes = require('./src/routes/paciente-route')
 const server = express()
 const PORT = 3007
+
+app.use(express.json());
+app.use('pessoas', userRoutes)
 
 server.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`))
