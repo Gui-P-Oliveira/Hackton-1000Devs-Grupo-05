56cc876945b4f433a95c2b7fc0e44758d4438d92
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-12-10 16:21:47 -0300

    feat: adiciona rotas vacina

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const pacienteRoute = require('./src/routes/paciente-route')
 const vacinaAplicadaRoute = require('./src/routes/vacina-aplicada-route')
+const consultaVacinaRoute = require('./src/routes/consulta-vacina-route')
 const server = express()
 const PORT = 3007
 server.use(express.json());
 server.use('/pessoas', pacienteRoute)
 server.use('/vacina-aplicada', vacinaAplicadaRoute)
+server.use('/consulta-vacina', consultaVacinaRoute)
 
 server.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`))
