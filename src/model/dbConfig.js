f8ae5a3fed0c36a12baa45cf213889e77f1f0e0b
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-12-09 22:46:41 -0300

    fix: retira console.log e conserta querys

--- src/model/dbConfig.js
+++ src/model/dbConfig.js
@@ -16,21 +16,23 @@ const showVacinasCount = async () => {
 //Cadastra Pacientes
 const cadastrarPaciente = async (nome, dataNascimento) => {
     const result = await pool.query('INSERT INTO PACIENTE (id_paciente, nome, Data_nascimento) VALUES ((SELECT COALESCE(MAX(id_paciente), 0) + 1 FROM PACIENTE), $1, $2) RETURNING *', [nome, dataNascimento]);
+    return result
 }
 //Consulta Paciente
-const consultaPaciente = async () => {
+const consultaPacientes = async () => {
     const result = await pool.query('SELECT * from PACIENTE');
-    console.log(result.rows);
+    return result
 }
 //Atualiza Paceiente 
 const atualizarPaciente = async (idPaciente, novoNome, novaDataNascimento) => {
     const result = await pool.query('UPDATE PACIENTE SET nome = $2, Data_nascimento = $3 WHERE id_paciente = $1 RETURNING *', [idPaciente, novoNome, novaDataNascimento]);
+    return result
 }
 
 //Consulta paciente por ID
-const pesquisaPaciente = async () => {
-    const result = await pool.query('SELECT * from PACIENTE');
-    console.log(result.rows);
+const pesquisaPaciente = async (id_paciente) => {
+    const result = await pool.query('SELECT * from PACIENTE WHERE id_paciente = $1', [id_paciente]);
+    return result
 }
 
 //Cadastrar rede
@@ -47,13 +49,15 @@ const consultaRede = async () => {
 //cadastrarIdRede('Rede Maluca')
 //consultaRede()
 
-//consultaPaciente() //Mudar nome da função
+// consultaPacientes() //Mudar nome da função
 
 //atualizarPaciente(53, 'Victor novo', '09/12/2023')
-consultaPaciente()
+
 module.exports = {
     cadastrarPaciente,
-    atualizarPaciente
+    atualizarPaciente,
+    consultaPacientes,
+    pesquisaPaciente
 
 }
 
