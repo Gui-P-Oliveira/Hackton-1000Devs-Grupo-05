5888e574137d17cc8a8ec3e9a22aee2f3ea9de55
Bowie <marketing@letscode.com.br>
2023-12-10 10:30:26 -0300

    feat: criação da rota Vacina Aplicadada

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const pacienteRoute = require('./src/routes/paciente-route')
+const vacinaAplicadaRoute = require('./src/routes/vacina-aplicada-route')
 const server = express()
 const PORT = 3007
 server.use(express.json());
 server.use('/pessoas', pacienteRoute)
+server.use('/vacina-aplicada', vacinaAplicadaRoute)
 
 server.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`))
